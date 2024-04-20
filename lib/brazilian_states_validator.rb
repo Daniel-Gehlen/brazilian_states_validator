beda248957780bf405badc82e23eeba8fb80847a
Daniel Gehlen <harmonia251251@gmail.com>
2024-04-20 01:22:19 -0300

    first commit

---
+++ lib/brazilian_states_validator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "brazilian_states_validator/version"
+
+module BrazilianStatesValidator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/brazilian_states_validator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BrazilianStatesValidator do
+  it "has a version number" do
+    expect(BrazilianStatesValidator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
